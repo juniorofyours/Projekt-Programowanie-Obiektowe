c7521e9ec4b3194ab1c95778b25e790746e9438d
Kamil <kamilkosmala06@wp.pl>
2026-05-31 22:58:47 +0200

    Vampire implementation added

--- src/main/java/org/example/Simulation.java
+++ src/main/java/org/example/Simulation.java
@@ -11,6 +11,8 @@ public class Simulation{
     private int numSteps;
     private int hour;
     private Boolean isCycling;
+    private int sunsetHour;
+    private int sunriseHour;
 
     public Simulation(int width, int height){
         board=new Board(width, height);
@@ -20,6 +22,8 @@ public class Simulation{
         numSteps=0;
         hour=0;
         container=null;
+        sunriseHour=6;
+        sunsetHour=18;
     }
 
     public void addAgent(Agent agent){ //dodaje agenta na planszę i do listy agents w symulacji
@@ -102,6 +106,17 @@ public class Simulation{
         if(isCycling==true) if(numSteps%50==0) {
             hour=(hour+1)%24;
         }
+    }
+
+    public int getCurrentHour(){
+        return hour;
+    }
+
+    public int getSunriseHour(){
+        return sunriseHour;
+    }
+    public int getSunsetHour(){
+        return sunsetHour;
     }
 
     private void step(){ //metoda oznaczająca jeden krok symulacji i wywołująca wszystkie meotdy
--- src/main/java/org/example/Vampire.java
+++ src/main/java/org/example/Vampire.java
@@ -1,4 +1,51 @@
 package org.example;
 
 public class Vampire extends Agent{
+    private Boolean hidden;
+    public Vampire(Simulation simulation, Board board, int x, int y, int energyBoost, int energyLoss){
+        super(simulation,board, x, y, energyBoost, energyLoss);
+        hidden=false;
+
+    }
+    public void updateCurrentState(){
+        if(simulation.getCurrentHour()==simulation.getSunriseHour()) hide();
+        else if(simulation.getCurrentHour()==simulation.getSunsetHour()) showUp();
+    }
+
+    private void hide(){
+        hidden=true;
+        board.removeFromBoard(this);
+
+    }
+    private void showUp(){
+        hidden=false;
+        board.addToBoard(this);
+    }
+
+    public Boolean isHidden(){
+        return hidden;
+    }
+
+    public void interact(){
+        if(hidden) return;
+        Cell cell=board.getCell(position.getX(), position.getY());
+        for(Agent agent : cell.getAgents()){
+            if(agent instanceof Human){
+                attack((Human)agent);
+            }
+        }
+        for(Garlic garlic : cell.getGarlics()){
+            getAttacked(garlic);
+        }
+    }
+
+    private void attack(Human human){
+        if(human.isSafe()) return;
+        boostEnergy(energyBoost);
+        human.loseEnergy(human.getEnergyLoss());
+    }
+    private void getAttacked(Garlic garlic){
+        simulation.removeGarlic(garlic);
+        loseEnergy(energyLoss);
+    }
 }
