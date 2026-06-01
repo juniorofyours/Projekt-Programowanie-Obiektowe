9a3df6c4cc6b9204d82b38cf9cee5fdde244ac19
yevhenii_tsaruk <yevhenii.tsaruk@gmail.com>
2026-06-02 01:53:04 +0200

    Skonczenie klasy Human oraz zrobienie czesci klasy TrainedHuman

--- src/main/java/org/example/Human.java
+++ src/main/java/org/example/Human.java
@@ -1,46 +1,69 @@
 package org.example;
 
 public class Human extends Agent{
+    /*zmienne instancyjne*/
     protected float transformationProb;
     protected float addProb;
     protected int stepsToReset;
     protected int lastGarlicStep;
     protected boolean safe;
 
-    //DODAC w przyszlosci do klasy z parametrami:
-    final float rangeToAdd= 10.0f; //albo np. = addProb+1
-    final float rangeToTransform = 10.0f;
+    /*DODAC w przyszlosci do klasy z parametrami:*/
+    final float rangeOfProbabilityOfAdd = 10.0f; //albo np. = addProb+1 //parametr zakresu z jakim prawdop. osoba moze rodzic
+    final float rangeOfProbabilityToTransform = 10.0f; //zakres prawdop. z jakim osoba moze sie zamienic w wampira
     final int stepsToGainDefence = 4; //liczba krokow po zjedzeniu czosnku, kiedy po nastepnym zjedzeniu czosnku,
                                       //można zyskać znowu ochronę
-    final int vampEnergyBoost=1000, vampEnergyLoss = 1000;
+    final int vampEnergyBoost=1000, vampEnergyLoss = 1000; //parametr z ktorym tworza sie wampiry
+    final int finalOfReset=3; //ile krokow zajmuje reset ochrony
+    final float addToAddProb=0.25f;
+    final float addToTransformProb=0.04f; //dodaje sie z kazdym krokiem do transformationProb
 
+    /*konstruktor*/
     public Human(Simulation simulation, Board board, int x, int y, float transformationProb, float addProb, int energyBoost, int energyLoss) {
         super(simulation, board, x, y, energyBoost, energyLoss); //tez ustawia np energyMax=1000 jak w konstruktorze klasy Agent
+        this.energyMax=1000;
+        this.energyLevel=1000;
+
         this.transformationProb=transformationProb;
         this.addProb=addProb;
 
-        //dane wartoci od początku są zerowe:
+        //wartosci poczatkowe
         this.lastGarlicStep=0;
         this.stepsToReset=0;
+        this.safe=false;
     }
 
-    //impelementacja metod, ktore w klasie Agent sa abstrakcyjne:
-    public void updateCurrentState(){
+    /*impelementacja metod, ktore w klasie Agent sa abstrakcyjne:*/
+    public void updateCurrentState(){ //metoda aktualizujaca stan czlowieka
+        //ochrona:
+        if (this.stepsToReset <= 0) {
+            this.safe=false;
+        }
+        else {
+            this.stepsToReset-=1;
+            this.safe=true;
+        }
+        //zwiekszenie sie szans zamiany sie w wampira:
+        this.transformationProb+=addToTransformProb;
+    }
 
-    } //metoda, która zostanie zaimplementowana w klasach
-    //    poszczególnych agentów aktualizująca ich stan
-    public void interact(){
+    public void interact(){ //metoda: interakcja człowieka z otoczeniem
+        Cell cell=board.getCell(this.position.getX(), this.position.getY());
+        for(Garlic garlic : cell.getGarlics()){
+            this.eat(garlic);
+        }
+    }
 
-    } //metoda, która zostanie zaimplementowana w klasach
-//    poszczególnych agentów przeprowadzająca ich interkacje z otoczeniem
-
+    /*pozostale metody*/
     //metoda z zadanym prawdopodbienstwem (addProb: float) dodaje czlowieka do planszy
+    //prawdopodobienstwo dziala na zasadzie, czy losuje sie liczba z jakiegos zakresu (rangeOfProbabilityOfAdd) i porownuje sie do
+    //zmiennej (addProb) danego obiektu, która się zwiększa z każdym krokiem (+=addToAddProb)
     public void tryAdd(){
 
         //czy osoba pojawi sie (czy zmienna losowa jest wieksza niz zadana wartosc)
-        if (rand.nextFloat(rangeToAdd) > this.addProb) {
+        if (rand.nextFloat(rangeOfProbabilityOfAdd) < this.addProb) {
 
-            //losowanie komórki, która jest dostępna (isUsable)
+            //losowanie komórki, która jest dostępna (isUsable)      //##dla przyszlych korekt: zostawiamy tak czy bez losowania?
             boolean statusCheck = false;
             int randX, randY;
             {
@@ -55,7 +78,9 @@ public class Human extends Agent{
 
             //dodanie nowego agenta na plansze
             this.board.addToBoard(newOne);
+            this.addProb=0;
         }
+        this.addProb+=addToAddProb;
     }
 
     public void eat(Garlic garlic){
@@ -64,9 +89,8 @@ public class Human extends Agent{
         this.boostEnergy(this.energyBoost);
 
         //usuniecie czosnku
-        Cell cellWithGarlic=board.getCell(this.getX(), this.getY());
-        cellWithGarlic.removeGarlic(garlic); //dla przyszlych korekt: czy dodajemy dany obiekt czosnku
-                                             // do kontenera czy zostawiamy gdzoes w pamieci?
+        Cell cellWithGarlic=board.getCell(this.position.getX(), this.position.getY());
+        cellWithGarlic.removeGarlic(garlic);
 
         //dodanie ochrony
         if(this.stepsToReset<=0) {
@@ -74,39 +98,29 @@ public class Human extends Agent{
             //ustawienie kroku, kiedy byl ostatnio wziety czosnek
             this.lastGarlicStep = simulation.getCurrentStep();
             //ustawienie liczby krokow z ochroną
-            this.stepsToReset = this.lastGarlicStep;
+            this.stepsToReset = finalOfReset;
         }
     }
 
     public boolean isSafe(){
-        //this.stepsToReset=
-        //sprawdza czy liczba krokow do anulowania ochrony danej przez czosnek
-        /*if (this.stepsToReset <= 0) {
-            this.safe=false;
-        }
-        else {
-            this.stepsToReset-=1;
-            this.safe=true;
-        }
-        */ //chyba to bedzie w updateCurrentState
         return this.safe;
     }
 
-    //nadpisane metody:
+    /*nadpisane metody:*/
     @Override
     public boolean tryRemove(){ //metoda uswająca agenta, jesli ma energię=0, powinna być nadpisana dla
                                 //ludzi by z danym prawdopodobienstwem zamienic ich w wampiry
         if(energyLevel==0) {
-            if(rand.nextFloat(rangeToTransform) > this.transformationProb) {
-                Agent newVampire = new Vampire(this.simulation, this.board, this.getX(), this.getY(), vampEnergyBoost, vampEnergyLoss);
+            if(rand.nextFloat(rangeOfProbabilityToTransform) < this.transformationProb) {
+                Agent newVampire = new Vampire(this.simulation, this.board, this.position.getX(), this.position.getY(), vampEnergyBoost, vampEnergyLoss);
                 this.board.addToBoard(newVampire);
             }
-            simulation.removeAgent(this);
+            //usuniecie osoby (Human)
+            this.board.removeFromBoard(this);
+            this.simulation.removeAgent(this);
             return true;
         }
         return false;
     }
 
 }
-//notatki odnosnie Human:
-//tryRemove() powinno byc nadpisane, poniewaz czlowiek moze zostac wampirem.
--- src/main/java/org/example/TrainedHuman.java
+++ src/main/java/org/example/TrainedHuman.java
@@ -1,4 +1,124 @@
 package org.example;
 
 public class TrainedHuman extends Human{
+    private float rectruitmentProb;
+    private float throwProb;
+    private int garlicStock;
+    private int garlicStockMax;
+    private int lastWeakeningStep;
+    private int stepsUntilGainingPower; //ile krokow zostalo do weakend=false
+    private boolean weakend;
+
+    /*DODAC w przyszlosci do klasy z parametrami:*/
+    final int stepsForGain = 2; //np    //dla stepsUntilGainPower
+    final float rangeOfProbabilityOfRecruitment = 10.0f; //zakres prawdop., z jakim osoba moze zostac zrekrutowana
+    final float addToRecruitmentProb=0.05f; //dodaje sie do recruitmentProb
+    final float rangeOfProbabilityOfThrow = 10.0f;
+    final float addToThrowProb=0.5f;
+
+    /*konstruktor*/
+    public TrainedHuman(Simulation simulation, Board board, int x, int y, float transformationProb, float addProb, float rectruitmentProb, float throwProb, int energyBoost, int energyLoss) {
+        super(simulation, board, x, y, transformationProb, addProb, energyBoost, energyLoss); //super() wywyoluje konsturktor
+                                         // klasy nadrzednej, wiec zmienne np energyMax lub np stepsToReset sa takie same (1000 i 0)
+        this.rectruitmentProb=rectruitmentProb;
+        this.throwProb=throwProb;
+
+        //wartosci poczatkowe
+        this.lastWeakeningStep=0;
+        this.stepsUntilGainingPower=0;
+        this.weakend=false;
+        this.garlicStock=0;
+        this.garlicStockMax=3; //np.
+
+    }
+
+    /*implementacja metod, ktore klasa TrainedHuman dziedziczy po klasie Human*/
+    //tryAdd() nie zmienia sie
+    //isSafe() nie zmienia sie
+    //eat() nie zmienia sie (poniewaz zmienna weakend sprawdza sie w metodzie interact)
+
+    @Override
+    public void updateCurrentState() {
+        //ochrona i prawdop. zamiany sie w wampira; //## dla przyszlych korekt: o ile jest taka sama dla wytrenowanej osoby
+        super.updateCurrentState();
+
+        //czy koniec trybu weakend:
+        if(this.stepsUntilGainingPower <= 0) {
+            this.weakend=false;
+        }
+        else {
+            stepsUntilGainingPower-=1;
+            this.weakend=true;
+        }
+    }
+
+    @Override
+    public void interact() {
+        Cell cell=board.getCell(this.position.getX(), this.position.getY());
+        if(!this.weakend) {
+            //zjedzenie czosnku
+            for (Garlic garlic : cell.getGarlics()) {
+                this.eat(garlic);
+            }
+            //rozrzucanie czosnku
+            if(this.garlicStock>0) {
+                this.tryThrow();
+            }
+        }
+        //rekrutowanie osob
+        for(Agent agent : cell.getAgents()){
+            if(agent instanceof Human){
+                this.tryRecruit((Human)agent);
+            }
+        }
+    }
+
+    /*pozostale metody*/
+
+    //metoda: prawdopodobienstwo rekrutacji osoby
+    //prawdop. dziala na zasadzie, ze zwieksza sie zmienna instancyjna recruitmentProb (dzieki stalej addToRecrutitmentProb)
+    //i jezeli zmienna jest wieksza niz zmienna losowa wylosowana z zakresu rangeOfProbabilityRecrutiment, to zachodzi rekrutacja
+    private void tryRecruit(Human human) {
+            if(rand.nextFloat(rangeOfProbabilityOfRecruitment) < this.rectruitmentProb) {
+                //throwProb i recruitmentProb = 0 dla nowej wyszkolonej osoby:
+                Agent newTrainedHuman = new TrainedHuman(this.simulation, this.board, this.position.getX(), this.position.getY(), human.transformationProb, human.addProb, 0, 0, human.energyBoost, human.vampEnergyLoss);
+
+                //dodanie osoby wyszkolonej
+                this.board.addToBoard(newTrainedHuman);
+
+                //usuniecie zwyklej osoby
+                this.board.removeFromBoard(human);
+                this.simulation.removeAgent(human);
+
+                this.rectruitmentProb=0;
+            }
+            this.rectruitmentProb+=addToRecruitmentProb;
+    }
+
+    //metoda: proba rozrzucenia czosnku
+    private void tryThrow() {
+        //warunek weakend i garlickStock!=0 jest sprawdzany wewnatrz interact()
+        if(rand.nextFloat(rangeOfProbabilityOfThrow) < this.throwProb) {
+            Cell cell = board.getCell(this.position.getX(), this.position.getY());
+
+            Garlic garlic = new Garlic(this.simulation, this.board, this.position.getX(), this.position.getY());
+            this.board.addGarlicToBoard(garlic);
+
+            this.garlicStock-=1;
+            this.weakend=true;
+            this.lastWeakeningStep=simulation.getCurrentStep();
+            this.throwProb=0;
+        }
+        this.throwProb+=addToThrowProb;
+    }
+
+    /*do zrobienia:*/
+    private void seekContiainer() {
+
+    }
+
+    private void collectGarlic() {
+
+    }
+
 }
